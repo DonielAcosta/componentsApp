885908f4b2bd22530aa557337e690be2726a3532
Doniel Acosta <donielacosta1995@gmail.com>
2025-04-18 11:01:17 -0400

    swtch y separardor componentes reutilizables

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,21 +1,42 @@
 /* eslint-disable react-native/no-inline-styles */
-import { Card } from '../../components/ui/Card';
 import React, { useState } from 'react';
-import { CustomView } from '../../components/ui/CustonView';
-import { Button } from '../../components/ui/Button';
+import { Card } from '../../components/ui/Card';
+import { CustomView } from '../../components/ui/CustomView';
+import { CustomSwitch } from '../../components/ui/CustomSwitch';
+import { Separator } from '../../components/ui/Separator';
 
 export const SwitchScreen = () => {
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  // const [isEnabled, setIsEnabled] = useState(false);
+  // const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const [state,setState] = useState({
+    isActive:true,
+    isHungry:false,
+    isHappy:true,
+  });
+
   return (
-    <CustomView style={{ marginTop:100 }}>
-        <Card>
-        {/* <Text>SwitchScreen</Text> */}
-        <Button
-            text="Click me"
-            onPress={ () =>{}}
-               />
-        </Card>
+    <CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}>
+      <Card>
+        <CustomSwitch
+          isOn={state.isActive}
+          onChange={(value)=> setState({...state, isActive:value})}
+          text="ESta activo ?"
+        />
+        <Separator/>
+        <CustomSwitch
+          isOn={state.isHungry}
+          onChange={(value)=> setState({...state, isHungry:value})}
+          text="Tiene  hambre"
+        />
+        <Separator/>
+
+        <CustomSwitch
+          isOn={state.isHappy}
+          onChange={(value)=> setState({...state, isHappy:value})}
+          text="Es Feliz?"
+        />
+      </Card>
     </CustomView>
   );
 };
